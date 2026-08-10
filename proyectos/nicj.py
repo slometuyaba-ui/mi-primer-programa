b12e7be47b824112cb39dfb5d1f1e693c463e840
slometuyaba-ui <slometuyaba@gmail.com>
2026-08-10 07:17:43 -0500

    Add files via upload

---
+++ proyectos/nicj.py
@@ -0,0 +1,14 @@
+type(23 )
+saludo="hola","MIUMDO"
+edad = 21
+
+ancho = 5
+alto = 20
+
+area = ancho * alto
+
+perimetro = 2 * ( ancho + alto)
+
+print ("area :" ,area )
+
+print("pemerimetro: " ,perimetro)
