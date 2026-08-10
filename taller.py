51c04c54f4e58304bb1dc1ea5c59cd7f7b4d54b0
slometuyaba-ui <slometuyaba@gmail.com>
2026-08-10 07:58:44 -0500

    Add files via upload

---
+++ taller.py
@@ -0,0 +1,12 @@
+numerico1 = 32
+numerico2 = 23
+suma = numerico1 + numerico2
+resta  = numerico1 - numerico2
+multiplicacion = numerico1 * numerico2
+division = numerico1 / numerico2
+
+print("suma:",suma)
+print("resta:",resta)
+print("multiplicacion:",multiplicacion)
+
+
